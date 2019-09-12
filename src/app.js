db4478a4bef72f3b9e1002a991a5bf0679f11a36
Soji Soyoye <soji.soyoye@gmail.com>
2019-09-12 15:37:45 +0100

    feat(graphql): set up graphql with dummy data
    - installed apollo server and other dependencies
    - setup app with graphql
    - created schema and resolvers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,34 @@
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+import cors from 'cors';
 import env from 'dotenv';
 import bodyParser from 'body-parser';
+
+import schema from './graphql/schema';
+import resolvers from './graphql/resolvers';
+import models from './models/db';
+
 
 env.config();
 const app = express();
 
-app.use(express.json());
+app.use(cors());
 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+    context: {
+        models,
+        me: models.users[1],
+    },
+})
+
+server.applyMiddleware({ app, path: '/graphql' });
 
 app.get('/', (req, res) => {
     return res.status(200).send({ 'message': 'Welcome to Antique™ World 🌍' });
@@ -19,6 +39,8 @@ app.all('*', (req, res) => res.status(404).send({
     message: 'This route does not exist'
 }));
   
-app.listen(3000)
-console.log('app running on port ', 3000);
+app.listen({ port: 3000 }, () => {
+    console.log('Apollo Server on http://localhost:3000/graphql');
+})
+
 
